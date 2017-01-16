b398fd739db194cbf20e3a10270dd822fcb13730
TMerrick55 <tmerrick44@gmail.com>
2017-01-16 16:15:27 -0500

    test 1 returns as 1

--- testmindminesfunction.rb
+++ testmindminesfunction.rb
@@ -1,7 +1,11 @@
 require 'minitest/autorun'
 
+def get_mined_mines_result(number)
+	1
+end
+
 class TestMMfunctions < Minitest::Test
 	def test_one_equals_one()
-		assert_equal(1,1)
+		assert_equal(1, get_mined_mines_result(1))
 	end
 end
