496f400705dfc7b62fd67323aca1720cca3590e4
TMerrick55 <tmerrick44@gmail.com>
2017-01-18 22:43:34 -0500

    7 function test HW

--- minedminesfunctions.rb
+++ minedminesfunctions.rb
@@ -1,3 +1,11 @@
 def get_mined_mines_result(number)
+	if number == 3
+		"mined"
+	else
+		number
+	end
+end
+
+def get_mined_minds_result (number)
 	number
 end
--- testmindminesfunction.rb
+++ testmindminesfunction.rb
@@ -9,4 +9,36 @@ class TestMMfunctions < Minitest::Test
 	def test_two_returns_two()
 		assert_equal(2, get_mined_mines_result(2))
 	end
+
+	def test_three_equals_mined()
+		assert_equal("mined", get_mined_mines_result(3))
+	end
+
+	def test_four_equals_mined_2()
+		assert_equal(4, get_mined_mines_result(4))
+	end
+
+	def test_five_returns_RWBY()
+		assert_equal(25, get_mined_mines_result(25))
+	end
+
+	def test_six_returns_Pyrrha()
+		assert_equal(7, get_mined_mines_result(7))
+	end
+
+	def test_seven_returns_forty()
+		assert_equal(4400, get_mined_mines_result(4400))
+	end
+
+	def test_eight_returns_JNPR()
+		assert_equal(1992, get_mined_mines_result(1992))
+	end
+
+	def test_nine_returns_red()
+		assert_equal(6, get_mined_mines_result(6))
+	end
+
+	def test_ten_returns_five()
+		assert_equal(50, get_mined_mines_result(50))
+	end
 end
